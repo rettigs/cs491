d6588ce5f36621c6b6cc7537dd54929e94c0dc11
Sean Rettig <seanrettig@gmail.com>
2014-10-13 11:54:17 -0700

    merge p3 into p2

--- p2/test.cpp
+++ p2/test.cpp
@@ -1,18 +1,25 @@
-#include "vec3.h"
+#include "mat4.h"
 
-int main(int argc, char *argv[]){
-    Vec3 a = Vec3(4.5, -5., 0);
-    Vec3 b = Vec3(8.222, 4.3, 3.15);
+int
+main( int argc, char *argv[ ] )
+{
+    Mat4 I;
+    I.Print( "I = " );
 
-               a.Print("    a =");
-               b.Print("    b =");
-           (a+b).Print("a + b =");
-           (a-b).Print("a - b =");
-            (-a).Print("   -a =");
-      (a.Unit()).Print("   a^ =");
-    (a.Cross(b)).Print("a x b =");
-                printf("a . b = %f\n", a.Dot(b));
-                printf("||a|| = %f\n", a.Length());
+    Mat4 M(  1.,  2.,  3.,  4.,   5.,  6.,  7.,  8.,   9., 10., 11., 12.,  13., 14., 15., 16. );
+    M.Print( "M = " );
+
+    Mat4 M2;
+    M2 = M;
+    M2.Print( "M2 = " );
+
+    Mat4 P = M * M;
+    P.Print( "P = " );
+
+        Vec3 V( 1., 2., 3. );
+        V.Print( "V =" );
+        Vec3 V1 = P * V;
+        V1.Print( "V1 =" );
 
     return 0;
 }
